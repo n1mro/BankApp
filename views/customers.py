a44fd79ab18d25f81259a89252859bffe0fb332f
n1mro <sohman89@gmail.com>
2022-01-23 12:10:10 +0100

    Fixed pagination listCustomers

--- views/customers.py
+++ views/customers.py
@@ -1,7 +1,6 @@
 from ast import Raise
 from logging import exception
 from flask import Blueprint, render_template, request
-from sqlalchemy import asc,desc
 from models import Customer
 from enum import Enum
 
@@ -12,27 +11,27 @@ class SortOrder(Enum):
     asc = "asc"
     desc = "desc"
 
-class ColumnToBeSorted(Enum):
+class CustomerColumn(Enum):
     Id = "Id"
     NationalId = "NationalId"
     GivenName = "GivenName"
     Streetaddress = "Streetaddress"
     Country = "Country"
 
-def sort_order_func(sort_order:SortOrder, column:ColumnToBeSorted):
+def sort_order_func(sort_order:SortOrder, column:CustomerColumn):
     column_to_be_sorted = select_column(column)
     return column_to_be_sorted.desc if sort_order==SortOrder.desc else column_to_be_sorted.asc
 
-def select_column(column:ColumnToBeSorted):
-    if column == ColumnToBeSorted.Id:
+def select_column(column:CustomerColumn):
+    if column == CustomerColumn.Id:
         return Customer.Id
-    elif column == ColumnToBeSorted.NationalId:
+    elif column == CustomerColumn.NationalId:
         return Customer.NationalId
-    elif column == ColumnToBeSorted.GivenName:
+    elif column == CustomerColumn.GivenName:
         return Customer.GivenName
-    elif column == ColumnToBeSorted.Streetaddress:
+    elif column == CustomerColumn.Streetaddress:
         return Customer.Streetaddress
-    elif column == ColumnToBeSorted.Country:
+    elif column == CustomerColumn.Country:
         return Customer.Country
     else:
         #Kontrollera detta!!!
@@ -46,7 +45,6 @@ def table_of_customers():
     q = request.args.get('q','')
 
     page = int(request.args.get('page',1))
-    active_page="table_of_customers"
 
     table_of_customer = Customer.query.filter(
         Customer.Id.like(f"%{q}%") |
@@ -56,18 +54,15 @@ def table_of_customers():
         Customer.Country.like(f"%{q}%") 
     )
 
-    sort_by = sort_order_func(SortOrder(sort_order),ColumnToBeSorted(sort_column))
+    sort_by = sort_order_func(SortOrder(sort_order),CustomerColumn(sort_column))
 
     table_of_customer = table_of_customer.order_by(sort_by())
     pagination_object = table_of_customer.paginate(page,20,False)
     
-    return render_template('customers/listCustomers.html', 
-            customer_list = pagination_object.items,
+    return render_template('customers/listCustomers.html',
             page=page,
             sort_order=sort_order,
             sort_column=sort_column,
             q=q,
-            has_next = pagination_object.has_next,
-            has_prev=pagination_object.has_prev,
-            pages=pagination_object.pages,
-            active_page=active_page)
+            pagination=pagination_object,
+            pages=pagination_object.pages)
