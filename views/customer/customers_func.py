c20a096ddefe427f023ab6c7726c78afaf55864c
n1mro <sohman89@gmail.com>
2022-02-16 22:56:09 +0100

    Clean up the structure of the project

---
+++ views/customer/customers_func.py
@@ -0,0 +1,39 @@
+from enum import Enum
+from models import Customer
+
+class ColumnError(Exception):
+    def __init__(self, message:str = "Not implemented or not a valid CustomerColumn variable!") -> None:
+        self.message = message
+        super().__init__(self.message)
+
+class SortOrderEnum(Enum):
+    asc = "asc"
+    desc = "desc"
+
+class CustomerColumnEnum(Enum):
+    Id = "Id"
+    NationalId = "NationalId"
+    GivenName = "GivenName"
+    Streetaddress = "Streetaddress"
+    Country = "Country"
+
+def sort_order_func(sort_order:SortOrderEnum, column:CustomerColumnEnum):
+    """Retruns a callable Customer colum sort order function"""
+    column_to_be_sorted = select_customer_column(column)
+    return column_to_be_sorted.desc if sort_order==SortOrderEnum.desc else column_to_be_sorted.asc
+
+def select_customer_column(column:CustomerColumnEnum):
+    """Returns a Customer colum"""
+    if column == CustomerColumnEnum.Id:
+        return Customer.Id
+    elif column == CustomerColumnEnum.NationalId:
+        return Customer.NationalId
+    elif column == CustomerColumnEnum.GivenName:
+        return Customer.GivenName
+    elif column == CustomerColumnEnum.Streetaddress:
+        return Customer.Streetaddress
+    elif column == CustomerColumnEnum.Country:
+        return Customer.Country
+    else:
+        raise ColumnError
+ 
